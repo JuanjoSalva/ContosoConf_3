642427df91d27fcab97d26b678d87d9f2d1a3458
jjsalva <jjsalva00@gmail.com>
2020-09-21 19:33:52 +0200

    segundo ejercico

--- ContosoConf/scripts/pages/schedule.js
+++ ContosoConf/scripts/pages/schedule.js
@@ -84,11 +84,17 @@
 
 // TODO: Task 2 - Get the "schedule" list element from the document
 // const list = ... ;
+const list = document.getElementById("schedule");
+const track1CheckBox = document.getElementById("show-track-1");
+const track2CheckBox = document.getElementById("show-track-2");
 
 function createSessionElement(session) {
     // TODO: Task 3 - Create a <li> element for the session.
     //       Add the session title as the <li> text content
     //       Return the <li> element
+    const li = document.createElement("li");    
+    li.textContent = session.title;
+    return li;
 };
 
 function clearList() {
@@ -103,9 +109,29 @@ function displaySchedule() {
     // TODO: Task 4 - Loop through the schedule array
     //       Create session elements
     //       Append the elements to the list   
+    for (let i = 0; i < schedule.length; i++) {
+        /*const li = createSessionElement(schedule[i]);
+        list.appendChild(li);*/
+        const tracks = schedule[i].tracks;
+        const isCurrentTrack = (track1CheckBox.checked && tracks.indexOf(1) >= 0) ||
+            (track2CheckBox.checked && tracks.indexOf(2) >= 0);
+        if (isCurrentTrack) {
+            const li = createSessionElement(schedule[i]);
+            list.appendChild(li);
+        }
+    }
+    //schedule.forEach((sche)=>list.appendchild(createsessionchild(sche)))
+
+    
 }
 
+
 displaySchedule();
+
+track1CheckBox.addEventListener("click", displaySchedule, false);
+track2CheckBox.addEventListener("click", displaySchedule, false);
+
+
 // SIG // Begin signature block
 // SIG // MIIaVgYJKoZIhvcNAQcCoIIaRzCCGkMCAQExCzAJBgUr
 // SIG // DgMCGgUAMGcGCisGAQQBgjcCAQSgWTBXMDIGCisGAQQB
